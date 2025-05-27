1d060e30eadd0ac538efa5012108f56d74a8e8f7
Emile Christie Dantas Pereira <emile.christie.pereira@ccc.ufcg.edu.br>
2025-05-27 11:03:29 -0300

    consertando de http pra https

--- src/service/axios.ts
+++ src/service/axios.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from "axios";
 
-const api_url = "http://eureca.lsd.ufcg.edu.br/graduacao-back/"
+const api_url = "https://eureca.lsd.ufcg.edu.br/graduacao-back/"
 
 export const axiosInstance = axios.create({
   baseURL: api_url,
